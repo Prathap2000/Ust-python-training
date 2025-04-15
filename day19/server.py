d9bfb3ccbc9852118e0b73ab3d13259c8989420a
prathap2000 <prathapj228@gmail.com>
2025-04-15 14:25:39 +0530

    chat app done

--- day19/server.py
+++ day19/server.py
@@ -5,5 +5,5 @@ s.listen(5) #number of connection
 while True:
     c,addr=s.accept() # connection establish with client
     print('got connection',addr)
-    c.send(bytes('thanks for connecting','utf-8'))
+    c.send(bytes('thanks for connecting','utf-8')) # to send the text throght the socket it need to convert to the bytes
     c.close() #close the connection
